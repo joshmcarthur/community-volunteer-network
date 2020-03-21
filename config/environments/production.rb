c840adf3713097bf690febe5345de9e112be3a90
Josh McArthur <joshua.mcarthur@gmail.com>
2020-03-21 22:31:15 +1300

    Configure outbound email more generically

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,10 +71,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Production email config
-  config.action_mailer.delivery_method = :postmark
-  config.action_mailer.postmark_settings = {
-    api_token: ENV.fetch("POSTMARK_API_KEY")
-  }
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = {
     host: "cvn.net.nz",
