2d092e9a5ba03bd79023e92fb4edc10772410318
Josh McArthur <joshua.mcarthur@gmail.com>
2020-03-22 17:34:02 +1300

    Allow account profile updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery prepend: true, with: :exception
   before_action :http_basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 
   private
 
